62170982e0cc9e0904b03e4b1ffbbc1fd003d43e
r1-covid2019 <61290683+r1-covid2019@users.noreply.github.com>
2020-02-21 03:40:28 +0900

    Add files via upload

---
+++ SEIR-model-diamon-prince.py
@@ -0,0 +1,49 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+# Total population, N.
+N = 3600
+# Initial number of exposed, infected and recovered individuals, I0 and R0.
+E0, I0, R0 =0, 1, 0
+# Everyone else, S0, is susceptible to infection initially.
+S0 = N - I0 - R0
+# Contact rate, beta, and mean recovery rate, gamma, (in 1/days). k from exposed (latent) to infected.
+beta, gamma , k = 0.9, 0.003 , 0.09
+# A grid of time points (in days)
+t = np.linspace(0, 60, 60)
+
+# The SIR model differential equations.
+def deriv(y, t, N, beta, gamma,k):
+    S, E, I, R = y
+    dSdt = -beta * S * I / N
+    dEdt = beta * S * I / N - k * E
+    dIdt = k * E - gamma * I
+    dRdt = gamma * I
+    return dSdt, dEdt, dIdt, dRdt
+
+# Initial conditions vector
+y0 = S0,E0, I0, R0
+# Integrate the SIR equations over the time grid, t.
+ret = odeint(deriv, y0, t, args=(N, beta, gamma, k))
+S,E,I,R = ret.T
+
+# Plot the data on three separate curves for S(t), I(t) and R(t)
+fig = plt.figure(facecolor='w')
+ax = fig.add_subplot(111, axisbelow=True)
+ax.set_facecolor('#dddddd')
+ax.plot(t, S, 'b', alpha=0.5, lw=2, label='Susceptible')
+ax.plot(t, E, 'y', alpha=0.5, lw=2, label='Exposed')
+ax.plot(t, I, 'r', alpha=0.5, lw=2, label='Infected')
+ax.plot(t, R, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+ax.set_xlabel('Time /days')
+ax.set_ylabel('Number')
+ax.set_ylim(0,4000)
+ax.yaxis.set_tick_params(length=0)
+ax.xaxis.set_tick_params(length=0)
+ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+legend = ax.legend()
+legend.get_frame().set_alpha(0.5)
+for spine in ('top', 'right', 'bottom', 'left'):
+    ax.spines[spine].set_visible(False)
+plt.show()
